bc5e71fa54555a49db8cb38163ed0f9798de94e6
Manny Toribio <manny.mtoribio@gmail.com>
2021-11-12 15:54:57 -0500

    deployed to app engine

---
+++ dist/src/gateway/dbConfig.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MONGO_URL = void 0;
+exports.MONGO_URL = "mongodb+srv://admin:Abc123@mindyourmoney.akmvs.mongodb.net/MindYourMoney?retryWrites=true&w=majority";
