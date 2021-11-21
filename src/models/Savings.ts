0d437c3cbe74a2c499710496feabf87102f19fb1
Manny Toribio <manny.mtoribio@gmail.com>
2021-11-21 12:59:55 -0500

    updated savings model

--- src/models/Savings.ts
+++ src/models/Savings.ts
@@ -3,6 +3,6 @@ import { ObjectId } from "mongodb";
 export interface Savings {
   _id?: ObjectId;
   savingsAmount: number;
-  updated_at: Date;
-  uid: string
+  updated_at?: Date;
+  uid?: string
 }
